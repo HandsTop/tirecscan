0337f6ed11870da7304ca8a6491d019fe7b3e28f
HandsTop <handstop0215@gmail.com>
2025-12-16 03:02:10 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBrQ-DDu9qbeteJQ804VIJ_r8XQ-2RjdA",
+  authDomain: "tire-shop-52e54.firebaseapp.com",
+  projectId: "tire-shop-52e54",
+  storageBucket: "tire-shop-52e54.firebasestorage.app",
+  messagingSenderId: "66109894982",
+  appId: "1:66109894982:web:bb7241c0c22bb6b8857547"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
