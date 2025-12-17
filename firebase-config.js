7d41d1363988920cf561bef2b75c1566a081a2df
HandsTop <handstop0215@gmail.com>
2025-12-17 05:51:35 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,23 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-  import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// firebase-config.js (работает в Safari/iPhone без сборщиков)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-  const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDBrQ-DDu9qbeteJQ804VIJ_r8XQ-2RjdA",
   authDomain: "tire-shop-52e54.firebaseapp.com",
   projectId: "tire-shop-52e54",
   storageBucket: "tire-shop-52e54.firebasestorage.app",
   messagingSenderId: "66109894982",
   appId: "1:66109894982:web:bb7241c0c22bb6b8857547"
-  };
+};
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
-  await signInAnonymously(auth);
-
-  console.log("🔥 Firebase подключён");
-</script>
+export async function ensureAuth() {
+  if (auth.currentUser) return auth.currentUser;
+  const cred = await signInAnonymously(auth);
+  return cred.user;
+}
