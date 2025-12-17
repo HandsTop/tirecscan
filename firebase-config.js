d61d597be9133a16d8ff2f3ab45d664759b5cc3b
HandsTop <handstop0215@gmail.com>
2025-12-17 04:07:49 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+  const firebaseConfig = {
   apiKey: "AIzaSyDBrQ-DDu9qbeteJQ804VIJ_r8XQ-2RjdA",
   authDomain: "tire-shop-52e54.firebaseapp.com",
   projectId: "tire-shop-52e54",
   storageBucket: "tire-shop-52e54.firebasestorage.app",
   messagingSenderId: "66109894982",
   appId: "1:66109894982:web:bb7241c0c22bb6b8857547"
-};
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth(app);
+
+  await signInAnonymously(auth);
+
+  console.log("🔥 Firebase подключён");
+</script>
