e3b2cd8486f408e13a6bd76af85ec5cb3446b07c
HandsTop <handstop0215@gmail.com>
2025-12-17 06:59:33 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
-import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-
 export const firebaseConfig = {
   apiKey: "AIzaSyDBrQ-DDu9qbeteJQ804VIJ_r8XQ-2RjdA",
   authDomain: "tire-shop-52e54.firebaseapp.com",
@@ -10,13 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: "66109894982",
   appId: "1:66109894982:web:bb7241c0c22bb6b8857547"
 };
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-
-export async function ensureAuth() {
-  if (auth.currentUser) return auth.currentUser;
-  const cred = await signInAnonymously(auth);
-  return cred.user;
-}
